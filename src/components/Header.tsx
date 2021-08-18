dea17b49408c56cbfe81a7e433ba50aa3d951bc3
ASUS-PC <setsko1@gmail.com>
2021-08-19 00:51:53 +0300

    New Components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+   return <header className="header">
+        <img src="https://99px.ru/sstorage/53/2016/10/tmb_181552_2066.jpg"/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+   return <div className="content">
+       <div>
+           <img src="https://ega.ee/wp-content/uploads/2020/06/cyber_hacker.jpg"/>
+       </div>
+       <div>
+           avatar + description
+       </div>
+       <div>
+           My posts
+           <div>
+               New posts
+           </div>
+           <div>
+               Post 1
+           </div>
+           <div>
+               Post 2
+           </div>
+       </div>
+   </div>
+}
+
+export default Profile;
